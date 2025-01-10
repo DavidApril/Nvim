81ce6ef310e52b8ffe0bc9797dd2bfa02cc924b2
David <davidagamez04@gmail.com>
2025-01-10 17:36:11 -0500

    feat: add basic keymaps

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+vim.keymap.set('n', '<Leader>bd', ':bd!<cr>', { desc = 'close current buffer' })
+vim.keymap.set('', '<Leader>rr', ':source %<cr>', { desc = 'source the current file'})
+vim.keymap.set('v', '>', '>gv', { desc = 'after tab in re-select the same' })
+vim.keymap.set('v', '<', '<gv', { desc = 'after tab out re-select the same' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = "Goes to the next result on the seach and put the cursor in the middle"})
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = "Goes to the prev result on the seach and put the cursor in the middle"})
