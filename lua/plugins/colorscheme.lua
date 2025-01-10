0bc8f9fb98f10d43801fdb84e40d1827fe56289d
David <davidagamez04@gmail.com>
2025-01-09 19:54:17 -0500

    feat: add oxocarbon colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  "nyoom-engineering/oxocarbon.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "oxocarbon"
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  end
+}
