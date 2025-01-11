8d3d4e19ae18244c9ae2f5d62157f15231bb257b
David <davidagamez04@gmail.com>
2025-01-11 17:37:55 -0500

    feat: configure lsp TypeScript language server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,8 +5,28 @@ return {
     'folke/neodev.nvim',
   },
   config = function()
+
     require('neodev').setup()
-    require('lspconfig').lua_ls.setup({
+    local lspconfig = require('lspconfig')
+
+    -- TypeScript
+    lspconfig.ts_ls.setup({
+     settings = {
+        completions = {
+          completeFunctionCalls = true,
+        },
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+      } 
+    })
+    -- Lua
+    lspconfig.lua_ls.setup({
       settings = {
         Lua = {
           telemetry = { enable = false },
