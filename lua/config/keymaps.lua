ba2307c226b121bf2d4c9f46fefeb1b8d6a67b45
David <davidagamez04@gmail.com>
2025-01-12 11:07:38 -0500

    feat: add keymaps for vertical and horizontal split window

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,17 @@ keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down",
 keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
+-- Move to window using the <ctrl> hjkl keys
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Windows
+keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+
 -- Resize window using <ctrl> arrow keys
 keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
